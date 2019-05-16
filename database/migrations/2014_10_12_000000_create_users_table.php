0c86f740deb2b3e3e0bc19597222f60d22f562d2
Gregorio <chiko150@outlook.com>
2019-05-16 22:17:33 +0700

    Using laravel\passport

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,8 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert(['name' => 'Administrator', 'email' => 'admin@eterno.loc', 'password' => bcrypt('secret')]);
     }
 
     /**
