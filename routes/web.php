9078d068c334f00fd430d7befc671796eb47b1e1
Gregorio Chiko Putra <chiko150@outlook.com>
2019-05-18 09:21:45 +0700

    Fixes conflicts

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,11 @@
 
 Route::get('/', function () {
     return view('welcome');
-<<<<<<< HEAD
-});
-
-Auth::routes();
-=======
 })->middleware('guest');
 
 //Auth::routes();
 Route::post('login', 'Auth\LoginController@authenticate')->name('login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::post('register', 'Auth\RegisterController@register')->name('register');
->>>>>>> d569e246a4c0a5bc4a366f9675b33b2435fde6c2
 
 Route::get('/home', 'HomeController@index')->name('home');
